dd66f05c65aba5f7e4bea4ac38347cc37418d9f8
Shteingolts <sergey.shteingolc@yandex.ru>
2022-08-30 19:23:10 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ SCREEN_WIDTH = 20
 SCREEN_HEIGHT = 20
 MAX_FOOD = 10
 
+
 class Snake():
     def __init__(self, x_coord=10, y_coord=10, direction='right', speed=SNAKE_SPEED, points=0) -> None:
         self.x = x_coord
@@ -23,7 +24,8 @@ class Snake():
         if 0 <= self.x <= Board.width*100 and 0 <= self.y <= Board.height*100:
             Board.board[round(self.x / 100)][round(self.y / 100)] = '@ '
         for snake_part in self.body:
-            Board.board[round(snake_part[0] / 100)][round(snake_part[1] / 100)] = '@ '
+            Board.board[round(snake_part[0] / 100)
+                        ][round(snake_part[1] / 100)] = '@ '
 
     def clear_tail(self, Board):
         if len(self.body) == 0:
@@ -34,16 +36,16 @@ class Snake():
     def move(self, Board, dt):
         y_max = Board.width * 100
         x_max = Board.height * 100
-        
+
         if self.direction == 'right' and self.y < y_max:
             self.y = self.y + self.speed * dt
         if self.direction == 'left' and 0 < self.y:
-            self.y = self.y - self.speed * dt            
+            self.y = self.y - self.speed * dt
         if self.direction == 'up' and 0 < self.x:
-            self.x = self.x - self.speed * dt            
+            self.x = self.x - self.speed * dt
         if self.direction == 'down' and self.x < x_max:
-            self.x = self.x + self.speed * dt           
-        
+            self.x = self.x + self.speed * dt
+
         if self.y > y_max:
             self.y = y_max
         if self.y < 0:
@@ -60,8 +62,9 @@ class Board():
         self.height = height
         self.max_food = 10
         self.food = []
-        self.board = [['. ' for k in range(self.width + 1)] for m in range(self.height + 1)]
-    
+        self.board = [['. ' for k in range(self.width + 1)]
+                      for m in range(self.height + 1)]
+
     def spawn_food(self):
         while len(self.food) < 10:
             x = random.randint(0, self.width)
@@ -72,12 +75,12 @@ class Board():
     def draw(self, snake, dt):
         to_draw = ''
         to_draw = to_draw + '--' * (self.width+1)
-        
+
         for row in self.board:
             to_draw = to_draw + '\n'
             for cell in row:
                 to_draw = to_draw + cell
-        
+
         to_draw = to_draw + '\n' + ('--' * (self.width+1)) + '\n'
         to_draw = to_draw + (f'Points: {snake.points}')
         print(to_draw)
@@ -86,7 +89,7 @@ class Board():
 def update(snake, board, dt):
 
     board.spawn_food()
-    
+
     if keyboard.is_pressed('right'):
         snake.direction = 'right'
     if keyboard.is_pressed('left'):
@@ -95,10 +98,10 @@ def update(snake, board, dt):
         snake.direction = 'up'
     if keyboard.is_pressed('down'):
         snake.direction = 'down'
-    
+
     if (round(snake.x / 100), round(snake.y / 100)) in board.food:
         snake.points += 1
-        board.food.remove((round(snake.x / 100), round(snake.y / 100)))  
+        board.food.remove((round(snake.x / 100), round(snake.y / 100)))
 
     snake.clear_tail(board)
     snake.move(board, dt)
@@ -108,9 +111,11 @@ def update(snake, board, dt):
         os.system('cls')
         sys.exit()
 
+
 def clear_screen():
     print('')
     print('\033[' + str(SCREEN_HEIGHT + 7) + 'A\033[2K', end='')
+
 
 def main():
     os.system('cls')
@@ -126,6 +131,6 @@ def main():
         update(snake, board, dt)
         board.draw(snake, dt)
 
-                   
+
 if __name__ == "__main__":
     main()
